7e73bad510dda32dd8406dca8a8971748800ec55
Chibu <atuchibueze@gmail.com>
2022-12-21 02:26:23 -0500

    create button component for business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we’ll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+
+        <Button styles="mt-10" />
+      </div>
+    </section>
+  );
 };
 
 export default Business;
