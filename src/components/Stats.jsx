1314f9d96023a51ad00a689578ea82bf5d5848cf
Chibu <atuchibueze@gmail.com>
2022-12-21 01:19:08 -0500

    implement hero component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      Stats
+    </section>
+  );
 };
 
 export default Stats;
